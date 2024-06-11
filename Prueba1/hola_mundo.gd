7bf405a0f171985d4bd4a85661f6d1385d6d212d
GDnickJB29 <114715028+GDnickJB29@users.noreply.github.com>
2024-06-11 09:22:04 -0600

    prueba_comandos
    prueba con gdscript

---
+++ Prueba1/hola_mundo.gd
@@ -0,0 +1,33 @@
+extends Node
+
+var valor_inicial = 200
+var intervalo = [10, 100]
+var suma = 1
+var resta = 1
+var switch = true
+
+
+func _ready():
+	pass
+
+func sumar(inicial, valor_a_sumar):
+	inicial += valor_a_sumar
+	return inicial
+
+func restar(inicial, valor_a_restar):
+	inicial -= valor_a_restar
+	return inicial
+
+func _process(_delta):
+	if switch == true:
+		print(valor_inicial)
+		valor_inicial = sumar(valor_inicial, suma)
+		if valor_inicial >= intervalo[1]:
+			switch = false
+	elif switch == false:
+		print(valor_inicial)
+		valor_inicial = restar(valor_inicial, resta) 
+		if valor_inicial <= intervalo[0]:
+			switch = true
+	
+	pass
